ae579836945dfa0b2e3c945a2806f9f8db21a761
magnoazneto <magnoazneto@gmail.com>
2021-05-07 14:05:58 -0300

    [refactor] ajusta alguns enderecos para variavels de ambiente

--- src/main/java/zupacademy/magno/propostas/sistemasexternos/analises/AnaliseRestricaoClient.java
+++ src/main/java/zupacademy/magno/propostas/sistemasexternos/analises/AnaliseRestricaoClient.java
@@ -5,11 +5,11 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "solicitacao", url = "http://127.0.0.1:9999/api")
+@FeignClient(value = "solicitacao", url = "${analises.host}")
 @Component
 public interface AnaliseRestricaoClient {
 
-    @RequestMapping(method = RequestMethod.POST, value = "/solicitacao", produces = "application/json")
+    @RequestMapping(method = RequestMethod.POST ,produces = "application/json")
     public AnaliseRestricaoResponse analisaRestricao(AnaliseRestricaoRequest request);
 
 }
--- src/main/java/zupacademy/magno/propostas/sistemasexternos/cartoes/CartoesClient.java
+++ src/main/java/zupacademy/magno/propostas/sistemasexternos/cartoes/CartoesClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "cartoes", url = "http://localhost:8888/api/cartoes")
+@FeignClient(value = "cartoes", url = "${cartoes.host}")
 @Component
 public interface CartoesClient {
 
